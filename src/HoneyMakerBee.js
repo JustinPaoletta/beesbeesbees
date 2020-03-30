e21699dbce64459505e3042e22a8264944a9c408
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-30 16:11:23 -0400

    made the beesknees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, color, food) {
+  Bee.call(this, age, color, food);
+  this.age = 10;
+  this.job = 'make honey'
+  this.eat;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot --;
+}
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function(age, color, food) {
+  ForagerBee.call(this, age, color, food);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead'
+};
+
+RetiredForagerBee.prototype.gamble = function(coin) {
+  this.treasureChest.push(coin);
+};
